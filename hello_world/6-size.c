c0907caa561a3029d7584186af49659a87f3ce9b
root <root@ip-10-42-117-57.ec2.internal>
2025-08-25 10:06:09 +0000

    new file

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(myChar));
 	printf("Size of an int: %lu byte(s)\n", sizeof(myInt));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(myLongInt));
-	printf("Size of a long long: %lu byte(s)\n", sizeof(myDouble));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(myDouble));
 	printf("Size of a float: %lu byte(s)\n", sizeof(myFloat));
 	return (0);
 }
