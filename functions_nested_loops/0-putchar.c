0748b417219583efb9c688d9a413672b6658120b
khalid <khalidalomari199@gmail.com>
2025-08-29 15:56:57 +0000

    new file

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int putcar(void)
+{
+	printf("_putchar");
+	return (0);
+}
+
+int main(void)
+{
+	putcar();
+	printf("\n");
+	return (0);
+}
