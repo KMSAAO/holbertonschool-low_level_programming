3abfc114339bfa8487323e2ee25142f05f477f16
kk <you@kkdkk.com>
2025-08-29 21:06:14 +0000

    new file

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+/** 
+ * _islower - entry point
+ *
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+	return (1);
+	}
+	else
+	{
+	return(0);
+	}
+}
