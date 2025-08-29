1bf1bade2b7e448731359e58eb2ce10685740b2b
kk <you@kkdkk.com>
2025-08-29 21:20:15 +0000

    new file

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - entry point
+ * Description: function type 1 if is lower and 0 if its A
+ * @c: its int
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+        if (isalpha(c))
+        {
+        return (1);
+        }
+        else
+        {
+        return (0);
+        }
+}   
