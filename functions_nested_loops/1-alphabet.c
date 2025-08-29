e829769a7ca1b8e69d0420d1f064fa837f413191
khalid <khalidalomari199@gmail.com>
2025-08-29 18:25:49 +0000

    new file

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
