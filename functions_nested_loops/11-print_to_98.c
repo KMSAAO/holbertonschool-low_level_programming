0ddc643864c17b6b8809df55eb3955a00df7d048
kk <kha@kd.com>
2025-08-30 16:48:36 +0000

    new file

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * print_to_98 - entry poitn
+ * @n: int
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+	for (; n <= 98; n++)
+	{
+	printf("%d", n);
+	if (n != 98)
+	printf(", ");
+	}
+	}
+	else
+	{
+	for (; n >= 98; n--)
+	{
+		printf("%d", n);
+	if (n != 98)
+		printf(", ");
+	}
+	}
+	printf("\n");
+}
+
