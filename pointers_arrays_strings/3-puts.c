fd7b19be1ce02e07219448719e6008b817b92bf4
Your Name <you@example.com>
2025-09-06 14:39:36 +0000

    new file

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - entry point
+ *
+ * @str: char
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
