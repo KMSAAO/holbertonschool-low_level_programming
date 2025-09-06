8d63bec99c1c0471e8b7163a807b8aaabb98c861
Your Name <you@example.com>
2025-09-06 16:39:15 +0000

    new file

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,25 @@
-#include "main.h"
-
 /**
  * _atoi - converts a string to an integer
- * @s: string to convert
- * Return: integer value
+ * @s: string to be converted
+ *
+ * Return: the integer value, or 0 if no valid number is found
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
 
-	while (s[i] != '\0')
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
-		else if (s[i] == '+')
-			;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			while (s[i] >= '0' && s[i] <= '9')
-			{
-				if (sign == 1 && result > (2147483647 - (s[i] - '0')) / 10)
-					return (2147483647);
-				if (sign == -1 && result > (2147483648U - (s[i] - '0')) / 10)
-					return (-2147483648);
-
-				result = result * 10 + (s[i] - '0');
-				i++;
-			}
-			return (result * sign);
-		}
 		i++;
 	}
-	return (0);
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	return (result * sign);
 }
