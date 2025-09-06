3229463bb0cd1200bfad544ccbc78d7f0e96fc36
Your Name <you@example.com>
2025-09-06 14:16:39 +0000

    new file

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen - entry point
+ *
+ * @s: itn
+ *
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
