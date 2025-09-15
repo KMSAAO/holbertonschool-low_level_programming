2eac74e6f2dbdf2d11c49780332af9499927f20f
Your Name <you@example.com>
2025-09-15 03:03:59 +0000

    new file

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array -  creates an array of chars
+ * @size: int
+ * @c: char
+ * Return: Pointer to the array or NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr = malloc(size * sizeof(char));
+
+	if (arr == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			arr[i] = c;
+		}
+	}
+
+	return (arr);
+
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,9 +1,8 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
 
-_putchar(char c);
+int _putchar(char c);
 
-//task 1
 char *create_array(unsigned int size, char c);
 
 #endif
