668a154779fb632ad016de8e44077df144e464ea
Your Name <you@example.com>
2025-09-15 07:38:06 +0000

    new file

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - entry point
+ * @width: int
+ * @height: int
+ * Return: int
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid = malloc(height * sizeof(int *));
+	int h, w;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	if (grid == NULL)
+		return (NULL);
+
+	for (h = 0; h < height; h++)
+	{
+		grid[h] = malloc(width * sizeof(int *));
+		if (grid[h] == NULL)
+			return (NULL);
+		for (w = 0; w < width; w++)
+		{
+			grid[h][w] = 0;
+		}
+	}
+	return (grid);
+}
