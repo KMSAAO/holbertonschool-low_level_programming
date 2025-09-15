0a6d285993c0315613901095e3b6137170efd18f
Your Name <you@example.com>
2025-09-15 03:36:39 +0000

    new file

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - entry point
+ * @str: string
+ * Return: NULL if str = 0 or Return array
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int j;
+	char *arr = malloc(i * sizeof(char));
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	i += 1;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; j < i; j++)
+	{
+		arr[j] = str[j];
+	}
+
+	arr[j] = '\0';
+
+	return (arr);
+
+}
