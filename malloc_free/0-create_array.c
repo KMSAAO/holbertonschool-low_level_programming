b17b15b0b5c8ce021876904d09a4c30569b4eee4
Your Name <you@example.com>
2025-09-15 03:14:58 +0000

    new file

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,16 +12,15 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *arr = malloc(size * sizeof(char));
 
-	if (arr == NULL)
+	if (size == 0)
 	{
 		return (NULL);
 	}
-	else
+
+
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			arr[i] = c;
-		}
+		arr[i] = c;
 	}
 
 	return (arr);
