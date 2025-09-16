e454435db66c468abe5cc4e2a980b580b8475a56
khalid <khalid@alomari>
2025-09-16 12:49:46 +0000

    new file

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * init_dog - entry point
+ * @d: struct
+ * @name: char
+ * @age: float
+ * @owner: char
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		exit(98);
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
