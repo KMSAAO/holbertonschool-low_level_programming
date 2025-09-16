48ff47111a7e14d8ae6267df878ebb1892125b21
khalid <khalid@alomari>
2025-09-16 12:22:49 +0000

    new file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
-
-struct dog {
+/**
+ * struct dog - dog info
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ */
+struct dog
+{
 
 	char *name;
 	float age;
