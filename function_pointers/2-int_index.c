b3940192ac76e28e1b3bae03a8425800f3f9489a
Your Name <you@example.com>
2025-09-21 23:15:39 +0000

    new file

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index - search for an integer
+ * @array: the array search in
+ * @size: size of element want to search
+ * @cmp: function
+ * Return: the index of the first elemetn and return -1 if size <= 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+
+		i++;
+	}
+
+		return (-1);
+}
