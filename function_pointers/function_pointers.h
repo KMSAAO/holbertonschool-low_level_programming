5dbdc4cfad18801949e950c46e66b5da0a814b87
Your Name <you@example.com>
2025-09-21 22:50:19 +0000

    new file

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
 #ifndef MYHEADER_H
 #define MYHEADER_H
-
+#include <stddef.h>
 int _putchar(char c);
 
-void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
