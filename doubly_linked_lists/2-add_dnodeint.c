343764501a7ea74bf102ee2026f86410ed02bc0a
Your Name <you@example.com>
2025-10-13 16:27:11 +0300

    add add_dnodeint function

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * add_dnodeint - add a new node at beginning of the list
+ * @head: lists
+ * @n: number
+ * Return: the address of the new element
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (head == NULL)
+		return (0);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
