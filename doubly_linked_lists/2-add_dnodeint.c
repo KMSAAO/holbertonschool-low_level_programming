eb800d86528adf2130b04a9bafd6bf56c3e91464
Your Name <you@example.com>
2025-10-13 16:56:54 +0300

    aa

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,11 +13,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (head == NULL)
+	{
 		return (0);
-
+	}
+	if (new_node == NULL)
+	{
+		return (0);
+	}
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
-
+	
 	return (new_node);
 }
