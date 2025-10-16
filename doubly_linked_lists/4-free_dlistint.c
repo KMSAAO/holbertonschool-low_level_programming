d2888823cac4789873a171c9bedabe1e14f6828a
Your Name <you@example.com>
2025-10-16 15:42:04 +0300

    add free_dlitint function

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_dlistint - frees list
+ * *head: the list
+ * Return: nothing
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *temp = head;
+
+    while (head != NULL)
+	{
+        temp = head->next;
+        free(head);
+        head = temp;
+	}
+    while (head != NULL)
+    {
+        temp = head->prev;
+		free(head);
+        head = temp;
+	}
+}
