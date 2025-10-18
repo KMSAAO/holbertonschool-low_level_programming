6030aef3be3e48e48509b1212ce05ae7e14a5465
Khalid alomari <khalidalomari199@gmail.com>
2025-10-18 18:54:13 +0300

    lk

--- file_io/main.h
+++ file_io/main.h
@@ -1,13 +1,20 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+/* ===== Function Prototypes ===== */
+
+/* 0-read_textfile.c */
 ssize_t read_textfile(const char *filename, size_t letters);
 
+/* 1-create_file.c */
 int create_file(const char *filename, char *text_content);
 
+/* 2-append_text_to_file.c */
 int append_text_to_file(const char *filename, char *text_content);
 
-int main(int argc, char *argv[]);
-#endif
+#endif /* MAIN_H */
