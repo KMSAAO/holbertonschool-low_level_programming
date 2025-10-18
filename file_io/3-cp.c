0f659da99a9b1c7300079142391052147ec4a9b7
Khalid alomari <khalidalomari199@gmail.com>
2025-10-18 19:05:41 +0300

    jg

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,81 +1,103 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <stdlib.h>
 
-#define BUFFER_SIZE 1024
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * error_exit - print error message and exit
- * @code: exit code
- * @msg: error message format
- * @arg: string argument
- */
-void error_exit(int code, const char *msg, const char *arg)
+* create_buffer - Allocates 1024 bytes for a buffer.
+* @file: The name of the file buffer is storing chars for.
+*
+* Return: A pointer to the newly-allocated buffer.
+*/
+char *create_buffer(char *file)
 {
-	dprintf(STDERR_FILENO, msg, arg);
-	exit(code);
+char *buffer;
+
+buffer = malloc(sizeof(char) * 1024);
+
+if (buffer == NULL)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't write to %s\n", file);
+exit(99);
+}
+
+return (buffer);
 }
 
 /**
- * close_fd - close file descriptor and handle errors
- * @fd: file descriptor
- */
-void close_fd(int fd)
+* close_file - Closes file descriptors.
+* @fd: The file descriptor to be closed.
+*/
+void close_file(int fd)
 {
-	if (close(fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
+int c;
+
+c = close(fd);
+
+if (c == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
 }
 
 /**
- * main - copy the content of a file to another
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 on success
- */
+* main - Copies the contents of a file to another file.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: 0 on success.
+*
+* Description: If the argument count is incorrect - exit code 97.
+*              If file_from does not exist or cannot be read - exit code 98.
+*              If file_to cannot be created or written to - exit code 99.
+*              If file_to or file_from cannot be closed - exit code 100.
+*/
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, r, w;
-	char buffer[BUFFER_SIZE];
+int from, to, r, w;
+char *buffer;
 
-	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to\n", "");
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+buffer = create_buffer(argv[2]);
+from = open(argv[1], O_RDONLY);
+r = read(from, buffer, 1024);
+to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		close_fd(fd_from);
-		error_exit(99, "Error: Can't write to %s\n", argv[2]);
-	}
+do {
+if (from == -1 || r == -1)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't read from file %s\n", argv[1]);
+free(buffer);
+exit(98);
+}
 
-	while ((r = read(fd_from, buffer, BUFFER_SIZE)) != 0)
-	{
-		if (r == -1)
-		{
-			close_fd(fd_from);
-			close_fd(fd_to);
-			error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-		}
+w = write(to, buffer, r);
+if (to == -1 || w == -1)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't write to %s\n", argv[2]);
+free(buffer);
+exit(99);
+}
 
-		w = write(fd_to, buffer, r);
-		if (w == -1 || w != r)
-		{
-			close_fd(fd_from);
-			close_fd(fd_to);
-			error_exit(99, "Error: Can't write to %s\n", argv[2]);
-		}
-	}
+r = read(from, buffer, 1024);
+to = open(argv[2], O_WRONLY | O_APPEND);
 
-	close_fd(fd_from);
-	close_fd(fd_to);
-	return (0);
+} while (r > 0);
+
+free(buffer);
+close_file(from);
+close_file(to);
+
+return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -17,6 +17,6 @@ int create_file(const char *filename, char *text_content);
 /* 2-append_text_to_file.c */
 int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char *argv[]);
-void copy_file(const char *file_from, const char *file_to);
-void close_fd(int fd);
+void close_file(int fd);
+char *create_buffer(char *file);
 #endif /* MAIN_H */
