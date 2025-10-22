d3c17b03165f258b63a1d2c63da72d9a5f42c0c5
Your Name <you@example.com>
2025-10-22 17:35:01 +0300

    fix malloc return

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,14 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
-	table->size = size;
-	table->array = malloc(sizeof(hash_table_t *) * table->size);
-
 	if (table == NULL)
 	{
 		return (NULL);
 	}
-	else if (table->array == NULL)
+	table->size = size;
+	table->array = malloc(sizeof(hash_table_t *) * table->size);
+	if (table->array == NULL)
 	{
 		return (NULL);
 	}
