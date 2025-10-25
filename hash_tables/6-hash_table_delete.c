3096fbe73d02f8198ed1fc049b10a2180a3382ac
Your Name <you@example.com>
2025-10-25 19:47:08 +0300

    add hash_table_delete function

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: is the hash table
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *temp;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
